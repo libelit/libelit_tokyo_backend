b7894efbba4b797d03b127fd37e3e5d816a7ca93
ayano <ayano.osive@gmail.com>
2026-02-10 19:41:40 +0530

    updated cors config

--- config/cors.php
+++ config/cors.php
@@ -15,13 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie','storage/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:3000'),
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
